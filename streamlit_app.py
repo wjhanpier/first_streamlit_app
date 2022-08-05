532ef5935a8fc803a563960d0a2e778e83a28c8f
william jhanpier <97306377+wjhanpier@users.noreply.github.com>
2022-08-05 16:58:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title ('My Parents new - ')
 
@@ -6,3 +7,5 @@ streamlit.header('Menú de desayuno')
 streamlit.text('🥣 Omega 3 y avena con arándanos')
 streamlit.text('🥗 Batido de col rizada, espinacas y rúcula')
 streamlit.text('🐔 Huevo de gallinas camperas hervidas')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
