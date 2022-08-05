64ff971a89167e11d6ea2be3cce53f7b925ed54a
william jhanpier <97306377+wjhanpier@users.noreply.github.com>
2022-08-05 16:52:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
 streamlit.title ('My Parents new - ')
-streamlit.title ('My Parents new - -')
-streamlit.title ('My Parents new --- ')
+
+streamlit.header('Menú de desayuno')
+streamlit.text('Omega 3 y avena con arándanos')
+streamlit.text('Batido de col rizada, espinacas y rúcula')
+streamlit.text('Huevo de gallinas camperas hervidas')
