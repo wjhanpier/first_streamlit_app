add333c4d633946397b780ec30d88a4c17dc7acd
william jhanpier <97306377+wjhanpier@users.noreply.github.com>
2022-08-05 17:00:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-import pandas
 
 streamlit.title ('My Parents new - ')
 
@@ -8,6 +7,6 @@ streamlit.text('🥣 Omega 3 y avena con arándanos')
 streamlit.text('🥗 Batido de col rizada, espinacas y rúcula')
 streamlit.text('🐔 Huevo de gallinas camperas hervidas')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 streamlit.dataframe(mi_lista_de_frutas)
