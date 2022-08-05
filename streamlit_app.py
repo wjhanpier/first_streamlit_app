65aa815b0f3b9fd766873b36f5643855a79d79c9
william jhanpier <97306377+wjhanpier@users.noreply.github.com>
2022-08-05 17:01:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,4 @@ streamlit.text('🐔 Huevo de gallinas camperas hervidas')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(mi_lista_de_frutas)
+streamlit.dataframe(my_fruit_list)
