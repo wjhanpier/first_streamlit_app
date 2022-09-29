a0e18ad4828231504d238c389afd4b4e883dcd94
william jhanpier <97306377+wjhanpier@users.noreply.github.com>
2022-09-29 10:45:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,25 +25,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list (my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-#
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice: 
-      streamlit.error("Please select a fruit to get information.")
-    else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
- 
-except URLError as e:
-  streamlit.error()
-  
-streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
-# write your own comment - what does this do?
-streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
